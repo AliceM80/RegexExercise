ec5ec062cddcfb2e8210be93677494428cbde311
AliceM80 <a.mutke@hotmail.de>
2021-10-08 12:19:10 +0200

    solve the exercise

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # RegexExercise
+
+### Solution1 using rgex : preferred!!
+public static String findtheTrapS1 (String sourceText);
+
+return m.find() ? "Boo!" : "Relax, there is no trap"; OR
+
+if(m.find()){
+ return "BOO!";
+}else {
+return "Relax, there`no trap";
+}
+
+### Solution2 using indexOf:
+public static String findtheTrapS2(String sourceText);
+
+
+
+### Solution 3 using contains:
+public static String findtheTrapS3(String sourceText);
+if(sourcetext.toLowerCase().contains("trap)){
+sout("BOO !");
+return "BOO!";
+}else{
+sout ("Relax");
+return "Relax";
+}
+
---
+++ src/main/java/RegexExercise1.java
@@ -0,0 +1,55 @@
+package main.java;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class RegexExercise1 {
+    public static void main(String[] args) {
+
+String sourceText1 = "I have never seen a thin person drinking Diet Coke.";
+String sourceText2 = "We're gonna build a wall!";
+String sourceText3 = "Happy Thanksgiving to all--even the haters and losers!";
+
+System.out.println(removeVowels1(sourceText1));
+System.out.println(removeVowels2(sourceText2));
+System.out.println(removeVowels3(sourceText3));
+
+    }
+
+    public static String removeVowels1(String sourceText1) {
+
+        String str = sourceText1.toLowerCase();
+        String replace = "";
+        Pattern pt = Pattern.compile("[aeiou]");
+        Matcher mt = pt.matcher(str);
+
+        str = mt.replaceAll(replace);
+        return str;
+
+    }
+
+    public static String removeVowels2(String sourceText2) {
+
+        String str = sourceText2.toLowerCase();
+        String replace = "";
+        Pattern pt = Pattern.compile("[aeiou]");
+        Matcher mt = pt.matcher(str);
+
+        str = mt.replaceAll(replace);
+        return str;
+
+    }
+
+    public static String removeVowels3(String sourceText3) {
+
+        String str = sourceText3.toLowerCase();
+        String replace = "";
+        Pattern pt = Pattern.compile("[aeiou]");
+        Matcher mt = pt.matcher(str);
+
+        str = mt.replaceAll(replace);
+        return str;
+
+    }
+    }
+
